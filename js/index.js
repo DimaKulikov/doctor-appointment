60b174799a4561b1eec6f3d7d391870d3349128a
DimaKulikov <dmyukulikov@gmail.com>
2020-12-24 21:51:10 +0300

    feat: get closest clininc with gps

--- js/index.js
+++ js/index.js
@@ -23,52 +23,72 @@ const clinics = [
   {
     name: 'ГБУЗ г. Москвы "Городская поликлиника № 69 ДЗМ"',
     type: 'Городская поликлиника',
-    shortName: 'ГП69 - 2-я Владимирская, 31А'
+    shortName: 'ГП69 - 2-я Владимирская, 31А',
+    latitude: '55.755068',
+    longitude: '37.785052'
   },
   {
     name: 'ГБУЗ ГП № 69 ДЗМ Филиал № 1 (ГП № 30)',
     type: 'Городская поликлиника',
-    shortName: 'ГП69Ф1 - Федеративный просп., 31'
+    shortName: 'ГП69Ф1 - Федеративный просп., 31',
+    latitude: '55.754127',
+    longitude: '37.823509'
   },
   {
     name: 'ГБУЗ ГП № 69 ДЗМ Филиал № 2 (ГП № 73)',
     type: 'Городская поликлиника',
-    shortName: 'ГП69Ф2 - Плющева, 20'
+    shortName: 'ГП69Ф2 - Плющева, 20',
+    latitude: '55.740091',
+    longitude: '37.760863'
   },
   {
     name: 'ГБУЗ г. Москвы "Городская поликлиника № 66 ДЗМ"',
     type: 'Городская поликлиника',
-    shortName: 'ГП66 - Салтыковская, 11Б'
+    shortName: 'ГП66 - Салтыковская, 11Б',
+    latitude: '55.734765',
+    longitude: '37.862408'
   },
   {
     name: 'ГБУЗ ГП 66 ДЗМ филиал 1',
     type: 'Городская поликлиника',
-    shortName: 'ГП66Ф1 - Рудневка, 8'
+    shortName: 'ГП66Ф1 - Рудневка, 8',
+    latitude: '55.715915',
+    longitude: '37.889861'
   },
   {
     name: 'ГБУЗ ГП 66 ДЗМ филиал 2',
     type: 'Городская поликлиника',
-    shortName: 'ГП66Ф2 - Молдагуловой, 10А'
+    shortName: 'ГП66Ф2 - Молдагуловой, 10А',
+    latitude: '55.725858',
+    longitude: '37.806847'
   },
   {
     name: 'ГБУЗ ГП 66 ДЗМ филиал 4',
     type: 'Городская поликлиника',
-    shortName: 'ГП66Ф4 - Новокосинская, 42'
+    shortName: 'ГП66Ф4 - Новокосинская, 42',
+    latitude: '55.742630',
+    longitude: '37.874904'
   },
   {
     name: 'ГБУЗ ГП №175 ДЗМ',
     type: 'Городская поликлиника',
-    shortName: 'ГП175 - Челябинская, 16'
+    shortName: 'ГП175 - Челябинская, 16',
+    latitude: '55.776618',
+    longitude: '37.828937'
   },
   {
     name: 'ГБУЗ ГП № 175 ДЗМ филиал № 1 (ГП № 16)',
     type: 'Городская поликлиника',
-    shortName: 'ГП175Ф1 - Старый Гай, 5'
+    shortName: 'ГП175Ф1 - Старый Гай, 5',
+    latitude: '55.738617',
+    longitude: '37.830195'
   },
   {
     name: 'ГБУЗ ГП № 175 ДЗМ филиал № 3 (ГП № 130)',
     type: 'Городская поликлиника',
-    shortName: 'ГП175Ф3 - Молостовых, 7к2'
+    shortName: 'ГП175Ф3 - Молостовых, 7к2',
+    latitude: '55.760282',
+    longitude: '37.833006'
   }
 ]
 
@@ -90,6 +110,58 @@ let appointmentInfo = {
 //get appointment info from local storage on load
 if(localStorage.getItem('appointmentInfo')){
   appointmentInfo = JSON.parse(localStorage.getItem('appointmentInfo'));
+}
+
+// 
+getClosestClinic();
+
+function getClosestClinic() {
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    let minDistance = 99999;
+    let closestClinicID;
+    console.log(latitude, longitude)
+
+    clinics.forEach((el, i) => {
+      let distance = PythagorasEquirectangular(latitude, longitude, clinics[i].latitude, clinics[i].longitude);
+      console.log(clinics[i].latitude);
+      if (minDistance > distance) {
+        minDistance = distance;
+        closestClinicID = i;
+      }
+    });
+    console.log(/*clinics[*/closestClinicID/*].name*/);
+    appointmentInfo.clinicID = closestClinicID;
+    renderInfo();
+  }
+
+  function error() {
+    alert('error');
+  }
+
+  if (!navigator.geolocation) {
+    alert('Geolocation is not supported by your browser');
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+function Deg2Rad(deg) {
+  return deg * Math.PI / 180;
+}
+
+function PythagorasEquirectangular(lat1, lon1, lat2, lon2) {
+  lat1 = Deg2Rad(lat1);
+  lat2 = Deg2Rad(lat2);
+  lon1 = Deg2Rad(lon1);
+  lon2 = Deg2Rad(lon2);
+  var R = 6371; // km
+  var x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2);
+  var y = (lat2 - lat1);
+  var d = Math.sqrt(x * x + y * y) * R;
+  return d;
 }
 
 function setLocalStorage(){
