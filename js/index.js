84f85040658ef4e3d6925be4beeddc840f9fd7f4
DimaKulikov <dmyukulikov@gmail.com>
2020-12-25 13:10:41 +0300

    refactor: organize functions, add comments

--- js/index.js
+++ js/index.js
@@ -12,11 +12,13 @@ Functionality:
 * Entered data is saved to LocalStorage to be kept between page reloads
 
 * On page load:
-  - Get current date and day of the week and display them on page
-  - Check if any previously stored data exists (stored AppointmentInfo). If it does load it into memory (local AppointmentInfo)
-  - Get user gps location. If it's available, find the closest clinic, save it to local appointmentInfo and update stored appointmentInfo
-  - Render local appointmentInfo
-  - When any changes to local appointmentInfo are made, render them and save to LocalStorage
+  1. Get current date and day of the week and display them on page
+  2. Check if any previously stored data exists (stored AppointmentInfo). If it does load it into memory (local AppointmentInfo)
+  3. Get user gps location. If it's available, find the closest clinic, save it to local appointmentInfo and update stored appointmentInfo
+  4. Render local appointmentInfo
+  5. Populate select fields with options from hardcoded data
+  
+* When any changes to local appointmentInfo are made, render them and save to LocalStorage
 
 To decide:
 * Show stored OR current time on page on load. Subsequently - store time in localStorage or not (for now the time is stored, current time shown in the input field)
@@ -29,7 +31,7 @@ import { specialities } from './specialities.js';
 
 //page elements
 let showBtn = document.querySelector('.burger-btn'),
-  form = document.querySelector('form[name=appointmentInfoFrom]'),
+  inputForm = document.querySelector('form[name=appointmentInfoFrom]'),
   speciality = document.querySelector('.appointment-details__doc-speciality'),
   doctor = document.querySelector('.appointment-details__doc-name'),
   time = document.querySelector('.appointment-details__time'),
@@ -47,8 +49,6 @@ let modal = document.querySelector('.modal'),
   inputRoom = document.querySelector('.modal__input_field_room'),
   inputTime = document.querySelector('.modal__input_field_time');
 
-
-
 //appointment info
 let appointmentInfo = {
   specialityID: '1',
@@ -58,62 +58,96 @@ let appointmentInfo = {
   time: ''
 }
 
-//get appointment info from local storage on load
+// 1 Get current date and day of the week and display them on page
+setDate();
+
+// 2. Check if any previously stored data exists (stored AppointmentInfo). If it does load it into memory (local AppointmentInfo)
 if(localStorage.getItem('appointmentInfo')){
   appointmentInfo = JSON.parse(localStorage.getItem('appointmentInfo'));
 }
 
-// 
+// 3. Get user gps location. If it's available, find the closest clinic, save it to local appointmentInfo and update stored appointmentInfo
 getClosestClinic();
 
+// 4. Render local appointmentInfo
+renderInfo();
+
+// 5. Populate select fields with options from hardcoded data
+clinics.forEach((el, index) => {
+  let newOption = document.createElement('option');
+  newOption.text = el.shortName;
+  newOption.value = index;
+  inputClinic.appendChild(newOption);
+})
+specialities.forEach((el, index) => {
+  let newOption = document.createElement('option');
+  newOption.text = el;
+  newOption.value = index;
+  inputSpeciality.appendChild(newOption);
+})
+
+
+// Event Listeners
+showBtn.addEventListener('click', showModal);
+closeBtn.addEventListener('click', hideModal);
+inputForm.addEventListener('submit', updateInfo);
+document.addEventListener('DOMContentLoaded', hideSpinner, false);
+
+// Functions 
 function getClosestClinic() {
+  if (!navigator.geolocation) {
+    alert('Geolocation is not supported by your browser');
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
 
   function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+    const userLatitude = position.coords.latitude;
+    const userLongitude = position.coords.longitude;
     let minDistance = 99999;
     let closestClinicID;
-
     clinics.forEach((el, i) => {
-      let distance = PythagorasEquirectangular(latitude, longitude, clinics[i].latitude, clinics[i].longitude);
+      let clinicLatitude = clinics[i].latitude,
+        clinicLongitute = clinics[i].longitude;
+      let distance = getDistanceBetween(userLatitude, userLongitude, clinicLatitude, clinicLongitute);
       if (minDistance > distance) {
         minDistance = distance;
         closestClinicID = i;
       }
     });
     appointmentInfo.clinicID = closestClinicID;
-    renderInfo();
   }
 
   function error() {
-    alert('error');
+    alert('Unable to get GPS location');
   }
 
-  if (!navigator.geolocation) {
-    alert('Geolocation is not supported by your browser');
-  } else {
-    navigator.geolocation.getCurrentPosition(success, error);
+  function getDistanceBetween(lat1, lon1, lat2, lon2) {
+    lat1 = deg2Rad(lat1);
+    lat2 = deg2Rad(lat2);
+    lon1 = deg2Rad(lon1);
+    lon2 = deg2Rad(lon2);
+    let R = 6371,
+      x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2),
+      y = (lat2 - lat1),
+      d = Math.sqrt(x * x + y * y) * R;
+    return d;
   }
-}
 
-function Deg2Rad(deg) {
-  return deg * Math.PI / 180;
-}
-
-function PythagorasEquirectangular(lat1, lon1, lat2, lon2) {
-  lat1 = Deg2Rad(lat1);
-  lat2 = Deg2Rad(lat2);
-  lon1 = Deg2Rad(lon1);
-  lon2 = Deg2Rad(lon2);
-  var R = 6371; // km
-  var x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2);
-  var y = (lat2 - lat1);
-  var d = Math.sqrt(x * x + y * y) * R;
-  return d;
+  function deg2Rad(deg) {
+    return deg * Math.PI / 180;
+  }
 }
 
 function setLocalStorage(){
   localStorage.setItem('appointmentInfo', JSON.stringify(appointmentInfo));
+}
+
+function setDate() {
+  const currentTime = new Date();
+  const options = { weekday: 'long', month: 'long', day: 'numeric' };
+  let currentDate = currentTime.toLocaleDateString('ru-RU', options);
+  date.textContent = currentDate.charAt(0).toUpperCase() + currentDate.slice(1);
 }
 
 function updateInfo(event) {
@@ -139,62 +173,34 @@ function renderInfo(){
 
 
 
-// populate select fields
-clinics.forEach((el, index) => {
-  let newOption = document.createElement('option');
-  newOption.text = el.shortName;
-  newOption.value = index;
-  inputClinic.appendChild(newOption);
-})
-specialities.forEach((el, index) => {
-  let newOption = document.createElement('option');
-  newOption.text = el;
-  newOption.value = index;
-  inputSpeciality.appendChild(newOption);
-})
-
-//load last date from local storage
-if (localStorage.getItem('clinic')) {
-  clinicName.textContent = clinics[localStorage.getItem('clinic')].name;
-  clinicType.textContent = clinics[localStorage.getItem('clinic')].type;
-}
-if (localStorage.getItem('doctor')) {
-  doctor.textContent = localStorage.getItem('doctor');
-}
-
-// set current date and weekday
-function updateDate() {
-  const currentTime = new Date();
-  const options = { weekday: 'long', month: 'long', day: 'numeric' };
-  let currentDate = currentTime.toLocaleDateString('ru-RU', options);
-  date.textContent = currentDate.charAt(0).toUpperCase() + currentDate.slice(1);  
-}
-
-function showModal() {
-  let currentTime = new Date();  
-  let currentHour = currentTime.getHours().toString().length > 1 ? currentTime.getHours() : '0' + currentTime.getHours(),
-      currentMinute = currentTime.getMinutes().toString().length > 1 ? currentTime.getMinutes() : '0' + currentTime.getMinutes();
+function showModal() {  
   modal.classList.add('modal_open');
   inputClinic.value = appointmentInfo.clinicID;
   inputDoctor.value = appointmentInfo.doctor;
   inputRoom.value = appointmentInfo.room;
   inputSpeciality.value = appointmentInfo.specialityID;
-  inputTime.value = currentHour + ':' + currentMinute;
+  inputTime.value = getCurrentTime();
+
+  function getCurrentTime() {
+    // in 00:00 format
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours().toString().length > 1
+      ? currentTime.getHours()
+      : '0' + currentTime.getHours();
+    let currentMinute = currentTime.getMinutes().toString().length > 1
+      ? currentTime.getMinutes()
+      : '0' + currentTime.getMinutes();
+    return currentHour + ':' + currentMinute;
+  }
 }
 
 function hideModal() {
   modal.classList.remove('modal_open');
 }
 
-showBtn.addEventListener('click', showModal);
-closeBtn.addEventListener('click', hideModal);
-form.addEventListener('submit', updateInfo);
-updateDate();
-renderInfo();
 
 function hideSpinner(){
   document.querySelector('.loading-screen').classList.add('loading-screen_hidden');
 }
 
-document.addEventListener('DOMContentLoaded', hideSpinner, false);
 
