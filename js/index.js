1a272ab3d4ae140e92b8db4fba33c2fafa444945
DimaKulikov <dmyukulikov@gmail.com>
2020-12-25 12:16:28 +0300

    move hardcoded data into separate files

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 /*------------------------
 
 Functionality:
@@ -17,13 +18,14 @@ Functionality:
   - Render local appointmentInfo
   - When any changes to local appointmentInfo are made, render them and save to LocalStorage
 
-*
 To decide:
-* Show stored OR current time on page on load. Subsequently - store time in localStorage or not (for now the time is stored, current time shows in the input)
+* Show stored OR current time on page on load. Subsequently - store time in localStorage or not (for now the time is stored, current time shown in the input field)
 
 ------------------------*/
 
 
+import { clinics } from './clinics.js';
+import { specialities } from './specialities.js';
 
 //page elements
 let showBtn = document.querySelector('.burger-btn'),
@@ -45,85 +47,7 @@ let modal = document.querySelector('.modal'),
   inputRoom = document.querySelector('.modal__input_field_room'),
   inputTime = document.querySelector('.modal__input_field_time');
 
-//clinics
-const clinics = [
-  {
-    name: 'ГБУЗ г. Москвы "Городская поликлиника № 69 ДЗМ"',
-    type: 'Городская поликлиника',
-    shortName: 'ГП69 - 2-я Владимирская, 31А',
-    latitude: '55.755068',
-    longitude: '37.785052'
-  },
-  {
-    name: 'ГБУЗ ГП № 69 ДЗМ Филиал № 1 (ГП № 30)',
-    type: 'Городская поликлиника',
-    shortName: 'ГП69Ф1 - Федеративный просп., 31',
-    latitude: '55.754127',
-    longitude: '37.823509'
-  },
-  {
-    name: 'ГБУЗ ГП № 69 ДЗМ Филиал № 2 (ГП № 73)',
-    type: 'Городская поликлиника',
-    shortName: 'ГП69Ф2 - Плющева, 20',
-    latitude: '55.740091',
-    longitude: '37.760863'
-  },
-  {
-    name: 'ГБУЗ г. Москвы "Городская поликлиника № 66 ДЗМ"',
-    type: 'Городская поликлиника',
-    shortName: 'ГП66 - Салтыковская, 11Б',
-    latitude: '55.734765',
-    longitude: '37.862408'
-  },
-  {
-    name: 'ГБУЗ ГП 66 ДЗМ филиал 1',
-    type: 'Городская поликлиника',
-    shortName: 'ГП66Ф1 - Рудневка, 8',
-    latitude: '55.715915',
-    longitude: '37.889861'
-  },
-  {
-    name: 'ГБУЗ ГП 66 ДЗМ филиал 2',
-    type: 'Городская поликлиника',
-    shortName: 'ГП66Ф2 - Молдагуловой, 10А',
-    latitude: '55.725858',
-    longitude: '37.806847'
-  },
-  {
-    name: 'ГБУЗ ГП 66 ДЗМ филиал 4',
-    type: 'Городская поликлиника',
-    shortName: 'ГП66Ф4 - Новокосинская, 42',
-    latitude: '55.742630',
-    longitude: '37.874904'
-  },
-  {
-    name: 'ГБУЗ ГП №175 ДЗМ',
-    type: 'Городская поликлиника',
-    shortName: 'ГП175 - Челябинская, 16',
-    latitude: '55.776618',
-    longitude: '37.828937'
-  },
-  {
-    name: 'ГБУЗ ГП № 175 ДЗМ филиал № 1 (ГП № 16)',
-    type: 'Городская поликлиника',
-    shortName: 'ГП175Ф1 - Старый Гай, 5',
-    latitude: '55.738617',
-    longitude: '37.830195'
-  },
-  {
-    name: 'ГБУЗ ГП № 175 ДЗМ филиал № 3 (ГП № 130)',
-    type: 'Городская поликлиника',
-    shortName: 'ГП175Ф3 - Молостовых, 7к2',
-    latitude: '55.760282',
-    longitude: '37.833006'
-  }
-]
 
-//specialities
-const specialities = ['Врач-терапевт участковый',
-  'Врач общей практики',
-  'Врач-оториноларинголог',
-  'Врач-офтальмолог']
 
 //appointment info
 let appointmentInfo = {
